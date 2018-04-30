ab3dad93d7d07c549df596713192205a436f0c73
shuyulxf <shuyulxf@gmail.com>
2018-04-30 16:12:13 +0800

    aa

--- homework/spaceship/main.js
+++ homework/spaceship/main.js
@@ -1,25 +1,28 @@
 
 "use strict";
 /* data type*/
-let isVarType = function() {
+var isVarType = function() {
 
-    let types = ["Array", "Boolean", "Date", "Number", "Object", "RegExp", "String", "Window", "HTMLDocument"];
-    for (let i in types) {
-        let type = types[i];
-        isVarType["is" + type] = function(data) {
-            return !(!data) && Object.prototype.toString.call(data) === "[object " + type + "]";
-        }
+    var types = ["Array", "Boolean", "Date", "Number", "Object", "RegExp", "String", "Window", "HTMLDocument"];
+    for (var i in types) {
+        var type = types[i];
+        (function(t) {
+            isVarType["is" + t] = function(data) {
+                return !(!data) && Object.prototype.toString.call(data) === "[object " + t + "]";
+            }
+        })(type)
+        
     }
 }
 isVarType();
 
 /* extend obj */
-let extend = function(obj1, obj2) {
+var extend = function(obj1, obj2) {
     
     if (!isVarType.isObject(obj1)) obj1 = {};
     if (!isVarType.isObject(obj2)) return obj1;
 
-    for (let key in obj2) {
+    for (var key in obj2) {
         obj1[key] = obj2[key] === undefined && obj1[key] || obj2[key];
     }
 
@@ -28,9 +31,9 @@ let extend = function(obj1, obj2) {
 
 function SimpleDialog(opts) {
 
-    let that = this;
+    var that = this;
 
-    let options = {
+    var options = {
         $container: document.getElementsByTagName("body")[0],
         title: "Dialog Title!",
         hasTitle : false,
@@ -55,17 +58,17 @@ function SimpleDialog(opts) {
 SimpleDialog.prototype = {
     init: function(){
 
-        let that = this,
+        var that = this,
             $dialog = document.createElement("dialog");
         $dialog.className += "dialog ";
         
         $dialog.addEventListener("click", function(e){
   
-            let t = e.target,
+            var t = e.target,
                 cs = t.className;
             if (cs.indexOf("close") !== -1 && cs.indexOf("sure") !== -1 && cs.indexOf("cancel") !== -1) return;
             
-            let {hasCancel, cancelCallback, hasSure, sureCallback} = that.options;
+            var {hasCancel, cancelCallback, hasSure, sureCallback} = that.options;
             if (cs.indexOf("cancel") !== -1 && hasCancel && cancelCallback) cancelCallback();
             if (cs.indexOf("sure") !== -1 && hasSure && sureCallback) sureCallback();
 
@@ -79,17 +82,17 @@ SimpleDialog.prototype = {
     },
     hide: function(){
 
-        let $dialog = this.$dialog;
+        var $dialog = this.$dialog;
         $dialog.className = $dialog.className.replace("show", "");
     },
     showDialog: function() {
 
-        let {auto, delay} = this.options,
+        var {auto, delay} = this.options,
             that = this;
         this.$container.append(this.getInnerDialogNode());
         this.show();
 
-        let timer = 0;
+        var timer = 0;
         if (auto) timer = setTimeout(function(){
             that.hide();
             clearTimeout(timer);
@@ -97,26 +100,26 @@ SimpleDialog.prototype = {
     },
     getInnerDialogNode: function() {
 
-        let {hasTitle, title, type, html, hasClose, hasCancel, cancelBtn, hasSure, sureBtn} = this.options;
+        var {hasTitle, title, type, html, hasClose, hasCancel, cancelBtn, hasSure, sureBtn} = this.options;
 
-        let renderOpBtnHTML = function(){
-            let sureBtnHTML = `<button class='sure'>${sureBtn}</button>`,
+        var renderOpBtnHTML = function(){
+            var sureBtnHTML = `<button class='sure'>${sureBtn}</button>`,
                 cancelBtnHTML = `<button class='cancel'>${cancelBtn}</button>`;
             
             return (hasCancel || hasSure) ? `<div class="op-btns"> ${sureBtnHTML}${cancelBtnHTML}</div>` : "";
         }
 
-        let renderCloseHTML = function() {
+        var renderCloseHTML = function() {
             return hasClose ? `<span class="close"><i>|</i><i>|</i></span>` : "";
         }
        
-        let HTML = `<p class=${hasTitle && "title"}>${hasTitle ? title : ""}</p>
+        var HTML = `<p class=${hasTitle && "title"}>${hasTitle ? title : ""}</p>
                 <div class="dialog-main">${html ? html : ""}</div>
                 ${renderOpBtnHTML()}
                 ${renderCloseHTML()}
             `;
         
-        let $dialog = this.$dialog;
+        var $dialog = this.$dialog;
         $dialog.innerHTML = HTML;
         $dialog.className += type;
 
@@ -130,15 +133,15 @@ SimpleDialog.prototype = {
     }
 }
 
-let showDialog = function(opts) {
+var showDialog = function(opts) {
     
-    let auto = opts.auto;
+    var auto = opts.auto;
     if (!auto) opts.hasClose = true;
-    let dialog = new SimpleDialog(opts);
+    var dialog = new SimpleDialog(opts);
     dialog.showDialog();
 }
 
-let PARAMS = {
+var PARAMS = {
     "ws": 10,
     "hs": 10,
     "keys": ['a', 'm', 'r', 'u'],
@@ -162,21 +165,21 @@ let PARAMS = {
     ]
 }
 
-let KEYNUMS = {
+var KEYNUMS = {
     'a': 0,
     'm': 0,
     'r': 0,
     'u': 0
 }
 
-let USERSPACECOORD = null;
-let ROBOTSPACECOORDLIST = [];
+var USERSPACECOORD = null;
+var ROBOTSPACECOORDLIST = [];
 
 function Cell(opts) {
     
-    let that = this;
+    var that = this;
 
-    let options = {
+    var options = {
         x: 0,
         y: 0
     };
@@ -186,7 +189,7 @@ function Cell(opts) {
     this.x = options.x;
     this.y = options.y;
 
-    let cb = opts.callback;
+    var cb = opts.callback;
 
     this.init();
 }
@@ -194,7 +197,7 @@ function Cell(opts) {
 Cell.prototype = {
     init: function() {
 
-        let that = this,
+        var that = this,
             opts = that.options,
             $cell = document.createElement("td");
 
@@ -202,30 +205,30 @@ Cell.prototype = {
 
         $cell.innerHTML = "<input type='text' maxlength='1'/>"
 
-        let $c = opts.$container;
+        var $c = opts.$container;
         $c.append($cell);
 
 
-        let isKey = function(key) {
+        var isKey = function(key) {
       
-            let keys = PARAMS.keys,
+            var keys = PARAMS.keys,
                 len = keys.length;
-            for(let i = 0; i < len; i++) {
+            for(var i = 0; i < len; i++) {
                 if (key == keys[i]) return true;
             }
             return false;
         }
 
 
-        let $ipt = $cell.getElementsByTagName("input")[0];
+        var $ipt = $cell.getElementsByTagName("input")[0];
         $cell.addEventListener("keydown", function(e) {
             
             if (INITIDX != 0) return;
 
-            let key = e.key,
+            var key = e.key,
                 value = $ipt.value;
 
-            let selTxt = window.getSelection().toString();
+            var selTxt = window.getSelection().toString();
 
             if (value && value.length > 0 && !selTxt) return;
             if (isKey(selTxt)) {
@@ -236,14 +239,14 @@ Cell.prototype = {
 
             if (!isKey(key)) {
                 showDialog({
-                    html: `Error Input.Must Be One Of 'a','m','r','u'!`,
+                    html: "Error Input.Must Be One Of 'a','m','r','u'!",
                     type: "error"
                 });
             } else {
 
                 if (key == PARAMS.onlykey && KEYNUMS[key] > 0) {
                     showDialog({
-                        html: `Error Input, Only One 'u' is allowed!`,
+                        html: "Error Input, Only One 'u' is allowed!",
                         type: "error"
                     });
                 } else {
@@ -258,7 +261,7 @@ Cell.prototype = {
    
 
         $cell.addEventListener("keyup", function(e){
-            let code = e.keyCode,
+            var code = e.keyCode,
                 key = that.key;
             if (isKey(key) && !$ipt.value) {
                 that.key = '';
@@ -300,7 +303,7 @@ Cell.prototype = {
 }
 
 // init table with 10 * 10 size
-let STAGE = '',
+var STAGE = '',
     STAGENUM = 3,
     INITIDX = 0,
     $stage = document.getElementsByClassName("top")[0],
@@ -328,7 +331,7 @@ var updateRSNum = function() {
 }
 var initStatus = function() {
     if (INITIDX == 0) {
-        let className = $table.className.replace('show','') + " hide";
+        var className = $table.className.replace('show','') + " hide";
         $table.className = className;
     } else {
         $table.className += " show";
@@ -340,7 +343,7 @@ var initStatus = function() {
     $numForRS.innerHTML = KEYNUMS['r'];
 }
 
-let changeStage = function(idx) {
+var changeStage = function(idx) {
 
     if (idx != 0 && !idx || idx < 0) {
         showDialog({
@@ -364,22 +367,22 @@ let changeStage = function(idx) {
 }
 
 
-let init = function(p) {
+var init = function(p) {
 
     // init stage
     changeStage(INITIDX);
 
     // draw cells
-    let ws = p.ws,
+    var ws = p.ws,
         hs = p.hs,
         $cells = document.getElementsByClassName("cells")[0];
 
-    for (let i = 0; i < ws; i++) {
+    for (var i = 0; i < ws; i++) {
 
-        let $row = document.createElement("tr");
-        let row = new Array();
-        for (let j = 0; j < hs; j++) {
-            let cell = new Cell({
+        var $row = document.createElement("tr");
+        var row = new Array();
+        for (var j = 0; j < hs; j++) {
+            var cell = new Cell({
                 x: i,
                 y: j,
                 $container: $row
@@ -395,12 +398,12 @@ let init = function(p) {
 
 init(PARAMS);
 
-let setCellsReadOnly = function() {
-    let ws = PARAMS.ws,
+var setCellsReadOnly = function() {
+    var ws = PARAMS.ws,
         hs = PARAMS.hs;
 
-    for (let i = 0; i < ws; i++) {
-        for (let j = 0; j < hs; j++) {
+    for (var i = 0; i < ws; i++) {
+        for (var j = 0; j < hs; j++) {
             cells[i][j].setReadOnly();
         }
     }
@@ -410,7 +413,7 @@ let setCellsReadOnly = function() {
 $btn.addEventListener("click", function(e){
     switch(INITIDX) {
         case 0:
-            let msg = '';
+            var msg = '';
            if (KEYNUMS['u'] == 0){
                 showDialog({
                     html: "Please Place A User Spaceship!",
@@ -571,7 +574,6 @@ var move = function(d, cell) {
 
     des.updateUserCoord();
     TURN = 1; 
-    debugger;
     robotMove(d); 
 
     if (KEYNUMS['m'] == 0) {
@@ -735,7 +737,7 @@ document.addEventListener("keydown", function(e) {
     
     if (TURN != 0 || INITIDX != 1) return;
 
-    let key = e.key,
+    var key = e.key,
         moves = PARAMS.moves;
 
     switch(key) {
